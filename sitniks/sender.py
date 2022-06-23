324f9015f97543f82cd6ff8c344691d93c275701
kirichk <kirichk@gmail.com>
2022-06-23 22:30:36 +0300

    fix

--- sitniks/sender.py
+++ sitniks/sender.py
@@ -7,24 +7,25 @@ from loguru import logger
 
 dotenv_path = os.path.join(Path(__file__).parent.parent, 'config/.env')
 load_dotenv(dotenv_path)
-TELEGRAM_URL = os.getenv("JIVO_TELEGRAM_WEBHOOK_URL")
-VIBER_URL = os.getenv("JIVO_VIBER_WEBHOOK_URL")
+TELEGRAM_BOT_ID = os.getenv("TELEGRAM_BOT_ID")
+# TELEGRAM_URL = os.getenv("JIVO_TELEGRAM_WEBHOOK_URL")
+# VIBER_URL = os.getenv("JIVO_VIBER_WEBHOOK_URL")
 
 
-@logger.catch
-def get_token():
-    URL = 'https://api.sitniks.com/api/authorization/token'
-    auth_token = os.getenv("SITNIKS_SECRET")
-    input = {
-        "client_id": "480",
-        "secret": auth_token
-        }
-    hed = {'Authorization': 'Bearer ' + auth_token,
-           'content-type': 'application/json'}
-    x = requests.post(URL,
-                      json=input,
-                      headers=hed)
-    logger.info(x.json())
+# @logger.catch
+# def get_token():
+#     URL = 'https://api.sitniks.com/api/authorization/token'
+#     auth_token = os.getenv("SITNIKS_SECRET")
+#     input = {
+#         "client_id": "480",
+#         "secret": auth_token
+#         }
+#     hed = {'Authorization': 'Bearer ' + auth_token,
+#            'content-type': 'application/json'}
+#     x = requests.post(URL,
+#                       json=input,
+#                       headers=hed)
+#     logger.info(x.json())
 
 
 @logger.catch
@@ -42,5 +43,5 @@ def send_message_telegram(data):
     logger.info(x.json())
 
 
-if __name__ == '__main__':
-    send_message()
+# if __name__ == '__main__':
+#     send_message_telegram()
